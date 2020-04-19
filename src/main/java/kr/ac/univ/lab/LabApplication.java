94e06d2b7c70928cf38eb9a29516a2e5a6def52a
scribnote5 <scribnote5@gmail.com>
2020-04-19 22:47:16 +0900

    6. 게시판 개발(summernote 적용) - 2

--- src/main/java/kr/ac/univ/lab/LabApplication.java
+++ src/main/java/kr/ac/univ/lab/LabApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,6 +39,9 @@ public class LabApplication {
 	@Bean
 	public CommandLineRunner runner(NoticeBoardRepository noticeBoardRepository) {
 		return (args) -> {
+			/* 기존 데이터 모두 삭제 */ 
+			noticeBoardRepository.deleteAll();
+			
 			IntStream.rangeClosed(1, 200).forEach(index -> 
 				noticeBoardRepository.save(NoticeBoard.builder()
 					.title("게시글" + index)
